c8b66e3c5180256e55f97917c3f5777f085da487
mwombeki <mwombeki.fabian@me.com>
2016-02-19 14:40:00 -0500

    Final matrix cache funtion

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-#The funtion inv_matrix() takes a matrix in the current environement and assigns it into another environmet for storage
+#The funtion 'makeCacheMatrix' takes a matrix in the current environement and assigns it into another environmet for storage
 # Assigns  the input ot a new environemt
 # Sets an inverse of a matrix m
 # Gets the inverse of that matrix
 # Creates a list all the above created variables to be used by the next function
-inv_matrix <- function(x= matrix()){
+makeCacheMatrix <- function(x= matrix()){
        m <- NULL
         set <- function(y = matrix()) {
                 x <<- y
@@ -14,9 +14,9 @@ inv_matrix <- function(x= matrix()){
         getinverse <- function() m
         list(get=get, set = set, getinverse = getinverse, setinverse = setinverse)
 }
-# Function 'cached_matrix' gets an inverse of a matirx 'x$getinverse' from a list if it exists if not, 
+# Function 'cacheSolve' gets an inverse of a matirx 'x$getinverse' from a list if it exists if not, 
 # Calculates the inverse 'inverse2' from the new matix supplied
-cached_matrix <- function(x = matrix()) {
+cacheSolve <- function(x = matrix()) {
                 m <- x$getinverse()
                 if (!is.null(m)) {
                 message("This is a cached matrix result")
